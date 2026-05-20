113efa3b36e0c6f60c23dc373bd463998fa90377
saitama <saitamagriladao@gmail.com>
2026-05-19 21:50:05 -0300

    Estudos JavaScript
    -Variáveis
    -Operadores
    -Condições if e else

---
+++ Atividades/script.js
@@ -0,0 +1,17 @@
+/* como fazer comentario no código */
+/* SHIFT + ALT + A OU CTRL + ; */
+
+var x = 7;/* declarando uma variavel x recebe 7 */
+let y = 5;
+const z = 6;
+var nome = "Daniel";
+console.log(nome);
+console.log(y);
+console.log(x);
+console.log(z);
+
+var soma = x + y;
+var soma2 = z + nome;/* Qando é string - Concatenação */
+
+console.log(soma);
+console.log(soma2);
--- script.js
+++
@@ -1,5 +0,0 @@
-var x = 1;
-var y = 2;
-var nome = "Daniel"
-
-console.log(`O usuário ${nome} tem nota ${x+y}`);
