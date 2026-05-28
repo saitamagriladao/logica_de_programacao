aae3a257662577f4344ebd2512578e33bb62dcba
saitama <saitamagriladao@gmail.com>
2026-05-27 21:48:51 -0300

    ex021

--- Atividades/ex019/script.js
+++ Atividades/ex019/script.js
@@ -3,4 +3,20 @@ function media() {
     var numero1 = Number(window.prompt('Qual foi a primeira nota de ${nome}?'))
     var numero2 = Number(window.prompt('Alem de ${numero1}, qual foi a outra nota de ${nome}?'))
     var situacao = ''
+    var media = (numero1 + numero2) / 2
+    var resultado = document.getElementById('situacao')
+    resultado.innerHTML = `<p>Calculando a média final de <mark>${nome}</mark>.</p>`
+    resultado.innerHTML = `<p>As notas obtidas foram <mark>${numero1} e ${numero2}</mark>.</p>`
+    resultado.innerHTML = `<p>A média final será <mark>${media}</mark>.</p>`
+    
+    if (media >= 7) {
+        situacao = 'Aprovado'
+        resultado.innerHTML += '<p>O aluno ${nome} foi <span style="color: white; background-color: green;">${situacao}</span></p>'
+    } else if (media < 5.5) {
+        situacao = 'Reprovado'
+        resultado.innerHTML += '<p>O aluno ${nome} foi <span style="color: white; background-color: red;">${situacao}</span></p>'
+    } else if {
+        situacao = 'Recuperação'
+        resultado.innerHTML += '<p>O aluno ${nome} foi <span style="color: white; background-color: red;">${situacao}</span></p>'
+    }
 }
---
+++ Atividades/ex021/script.js
@@ -0,0 +1,43 @@
+function verificar(){
+    let nascimento = document.getElementById('nascimento').value; // menor de idade não pode doar sangue.
+
+    let doencaSelecionada = document.querySelector('input[name="sick"]:checked').value;
+
+    let peso = Number(document.getElementById('peso').value) // peso minimo é 50kg
+
+    let tattooSelecionada = document.querySelector('input[name="tattoo"]:checked').value;
+
+    let medicineSelecionada = document.querySelector('input[name="medicine"]:checked').value;
+
+    console.log(`Nacimento: ${nascimento} / Doença: ${doencaSelecionada} / Peso: ${peso} / Tatuagem: ${tattooSelecionada} / Doença: ${medicineSelecionada}`)
+
+    /* Capturar idade */
+    console.log(nascimento)
+    console.log(typeof nascimento)
+    let dataNascimento = new Date(nascimento + "T00:00:00")
+    console.log(nascimento)
+    console.log(typeof dataNascimento)
+
+    let hoje = new Date()
+    let idade = hoje.getFullYear() - dataNascimento.getFullYear();
+    console.log(idade)
+    let dia = dataNascimento.getDate();
+    let mes = dataNascimento.getMonth() + 1;
+
+
+    if (idade <= 17) {
+        resultado.innerHTML = `<p><span style="color: white; background-color: red;">Você é menor de idade e não pode doar sangue.</span></p>`
+    } else {
+        resultado.innerHTML = `<p><span style="color: white; background-color: green;">Você é maior de idade e pode doar sangue.</span></p>`
+
+    } if (doencaSelecionada = `yes`) {
+        resultado.innerHTML += `<p><span style="color: white; background-color: red;">Você possui alguma doença e não pode doar sangue.</span></p>`
+    } else {
+        resultado.innerHTML += `<p><span style="color: white; background-color: green;">Você não possui doença e pode doar sangue.</span></p>`    
+
+    } if (peso < 50) {
+        resultado.innerHTML += `<p><span style="color: white; background-color: red;">Você está abaixo do peso mínimo e não pode doar sangue.</span></p>`
+    } else {
+        resultado.innerHTML += `<p><span style="color: white; background-color: green;">Você está dentro da margem de peso mínimo e pode doar sangue.</span></p>`
+    }
+}
